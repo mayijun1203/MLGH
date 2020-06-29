1b9bafdd14d2274b16fc8199bd7f180dd02db03e
mayijun1203 <mayijun1203@gmail.com>
2020-06-29 11:43:52 -0400

    Update linear regression.py

--- linear regression/linear regression.py
+++ linear regression/linear regression.py
@@ -26,12 +26,24 @@ df.plot(x='original',y='sell',style='o',title='sell vs original')
 # Sklearn
 xtrain,xtest,ytrain,ytest=sklearn.model_selection.train_test_split(df[['original','age','km','petrol','dealer','manual']],df['sell'],test_size=0.5)
 reg=sklearn.linear_model.LinearRegression().fit(xtrain,ytrain)
-ypred=pd.DataFrame({'test':ytest,'pred':reg.predict(xtest)})
-ypred.plot(x='test',y='pred',style='o')
-print(sklearn.metrics.mean_absolute_error(ytest, ypred['pred']))
-print(sklearn.metrics.mean_squared_error(ytest, ypred['pred']))
-print(sklearn.metrics.mean_squared_error(ytest, ypred['pred']))
-print(sklearn.metrics.median_absolute_error(ytest, ypred['pred']))
+lasso=sklearn.linear_model.Lasso(alpha=0.1).fit(xtrain,ytrain)
+ypred=pd.DataFrame({'test':ytest,'pred':reg.predict(xtest),'lasso':lasso.predict(xtest)})
+ypred.plot(x='test',y=['pred','lasso'],style='o')
+print(sklearn.metrics.mean_absolute_error(ypred['test'], ypred['pred']))
+print(sklearn.metrics.mean_absolute_error(ypred['test'], ypred['lasso']))
+
+print(sklearn.metrics.mean_squared_error(ypred['test'], ypred['pred']))
+print(sklearn.metrics.mean_squared_error(ypred['test'], ypred['lasso']))
+
+print(sklearn.metrics.mean_squared_error(ypred['test'], ypred['pred']))
+print(sklearn.metrics.mean_squared_error(ypred['test'], ypred['lasso']))
+
+print(sklearn.metrics.median_absolute_error(ypred['test'], ypred['pred']))
+print(sklearn.metrics.median_absolute_error(ypred['test'], ypred['lasso']))
+
+
+
+
 
 
 
