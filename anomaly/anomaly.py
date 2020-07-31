d7e9dc39db7b7cebab798149d33c3f69badfcb1e
Yijun Ma <mayijun1203@gmail.com>
2020-07-30 23:35:05 -0400

    Update anomaly.py

--- anomaly/anomaly.py
+++ anomaly/anomaly.py
@@ -3,7 +3,7 @@ import numpy as np
 import sklearn.model_selection
 import sklearn.linear_model
 import statsmodels.api as sm
-
+import sklearn.ensemble
 
 
 df=pd.read_csv('C:/Users/mayij/Desktop/DOC/GITHUB/MLGH/logistic regression/bank.csv',sep=';')
@@ -20,48 +20,19 @@ df=df[['deposit','age','default','balance','housing','loan','campaign']].reset_i
 # take log or power for skewed data
 
 
-# Sklearn
+# Isolation Forest
 xtrain,xtest,ytrain,ytest=sklearn.model_selection.train_test_split(df[['age','default','balance','housing','loan','campaign']],df['deposit'],test_size=0.2)
-xtrain,xval,ytrain,yval=sklearn.model_selection.train_test_split(xtrain,ytrain,test_size=0.25)
-reg=sklearn.linear_model.LogisticRegression().fit(xtrain,ytrain)
-ypred=pd.DataFrame({'val':yval,'prob':[x[1] for x in reg.predict_proba(xval)],'pred':reg.predict(xval)})
+clf=sklearn.ensemble.IsolationForest(n_estimators=100,warm_start=True).fit(xtrain)
+
+ypred=pd.DataFrame({'train':ytrain,'pred':clf.fit_predict(xtrain)})
+
 ypred.plot(x='val',y='pred',style='o')
 print(sklearn.metrics.classification_report(yval, ypred['pred']))
 
 
 
-# Statsmodels for model summary
-sm.Logit(ytrain,sm.add_constant(xtrain)).fit().summary2()
 
 
-
-
-
-
-
-
-
-
-
-
-df=pd.read_csv('C:/Users/mayij/Desktop/DOC/GITHUB/IPFGH/HHTS.csv')
-df['SEX']=np.where(df['SEX']=='MALE',1,0)
-df['RACE1']=np.where(df['RACE']=='BLACK',1,0)
-df['RACE2']=np.where(df['RACE']=='ASIAN',1,0)
-df['RACE3']=np.where(df['RACE']=='OTHER',1,0)
-df['AGE1']=np.where(df['AGE']=='A25-34',1,0)
-df['AGE2']=np.where(df['AGE']=='A35-54',1,0)
-df['AGE3']=np.where(df['AGE']=='A55-64',1,0)
-df['AGE4']=np.where(df['AGE']=='A>=65',1,0)
-df['EMP']=np.where(df['EMPLY']=='Yes',1,0)
-df['TRIP']=np.where(df['PTRIPS']==1,1,np.where(df['PTRIPS']==2,2,np.where(df['PTRIPS']==3,3,np.where(df['PTRIPS']==4,4,5))))
-df=df[['SEX','RACE1','RACE2','RACE3','AGE1','AGE2','AGE3','AGE4','EMP','TRIP']].reset_index(drop=True)
-
-xtrain,xtest,ytrain,ytest=sklearn.model_selection.train_test_split(df[['SEX','RACE1','RACE2','RACE3','AGE1','AGE2','AGE3','AGE4','EMP']],df['TRIP'],test_size=0.4)
-
-reg=sklearn.linear_model.LogisticRegression().fit(xtrain,ytrain)
-ypred=pd.DataFrame({'test':ytest,'pred':reg.predict(xtest)})
-sm.MNLogit(ytrain,sm.add_constant(xtrain)).fit().summary()
 
 
 import plotly
