5b00556517c0c9ad92b27e70572474fee49be35e
Yijun Ma <mayijun1203@gmail.com>
2020-08-08 21:19:41 -0400

    update

--- pyspark/pyspark.py
+++ pyspark/pyspark.py
@@ -1,3 +1,4 @@
+# Test Pi
 import findspark
 findspark.init()
 
@@ -17,3 +18,46 @@ pi = 4 * count / num_samples
 print(pi)
 
 sc.stop()
+
+
+
+
+
+
+
+
+
+
+
+# Read csv
+import findspark
+findspark.init()
+
+import pyspark
+sc=pyspark.SparkContext(appName="fhv")
+spark=pyspark.sql.SparkSession(sc)
+
+df=spark.read.csv('C:/Users/mayij/Desktop/fhvhv_tripdata_2020-06.csv',header=True,inferSchema=True)
+df.show(5)
+df.printSchema()
+df.filter(df.hvfhs_license_num=='HV0005').show(2)
+df.where((df.hvfhs_license_num=='HV0005')&(df.PULocationID==55)).show(2)
+df.registerTempTable('dfsql')
+spark.sql('select * from dfsql').show(2)
+
+
+
+
+
+
+
+# Read RDD
+import findspark
+findspark.init()
+
+import pyspark
+sc=pyspark.SparkContext(appName="fhv")
+
+rdd=sc.textFile('hdfs://localhost:9000/fhv/fhvhv_tripdata_2020-06.csv')
+rdd.take(10)
+
