55dd9c905805a3bd92643cbba03d472cf588daa2
Yijun Ma <mayijun1203@gmail.com>
2020-08-21 22:03:46 -0400

    Update knn.py

--- knn/knn.py
+++ knn/knn.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import sklearn.model_selection
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 
 
-
+# Classification
 df=pd.read_csv('C:/Users/mayij/Desktop/DOC/GITHUB/MLGH/logistic regression/bank.csv',sep=';')
 df['deposit']=np.where(df['y']=='yes',1,0)
 df['default']=np.where(df['default']=='yes',1,0)
@@ -37,6 +38,42 @@ print(sklearn.metrics.classification_report(yval, ypred['pred']))
 
 
 
+
+
+
+
+
+
+# Regression
+df=pd.read_csv('C:/Users/mayij/Desktop/DOC/GITHUB/MLGH/linear regression/car.csv')
+df['sell']=df['Selling_Price']
+df['original']=df['Present_Price']
+df['age']=2019-df['Year']
+df['km']=df['Kms_Driven']/1000
+df['petrol']=np.where(df['Fuel_Type']=='Petrol',1,0)
+df['dealer']=np.where(df['Seller_Type']=='Dealer',1,0)
+df['manual']=np.where(df['Transmission']=='Manual',1,0)
+df=df[['sell','original','age','km','petrol','dealer','manual']].reset_index(drop=True)
+
+
+xtrain,xtest,ytrain,ytest=sklearn.model_selection.train_test_split(df[['original','age','km','petrol','dealer','manual']],df['sell'],test_size=0.2)
+xtrain,xval,ytrain,yval=sklearn.model_selection.train_test_split(xtrain,ytrain,test_size=0.25)
+
+
+score=pd.DataFrame()
+score['k']=range(1,21)
+score['trainscore']=np.nan
+score['valscore']=np.nan
+for i in list(score['k']):
+    knn=KNeighborsRegressor(n_neighbors=i).fit(xtrain,ytrain)
+    score.loc[i-1,'trainscore']=knn.score(xtrain,ytrain)
+    score.loc[i-1,'valscore']=knn.score(xval,yval)
+
+k=11
+knn=KNeighborsRegressor(n_neighbors=k).fit(xtrain,ytrain)
+
+ypred=pd.DataFrame({'test':ytest,'pred':knn.predict(xtest)})
+ypred.plot(x='test',y=['pred'],style='o')
 
 
 
