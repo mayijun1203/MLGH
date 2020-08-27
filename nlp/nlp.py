e2111b11dc9f26a3599fb3688fd8ef0a91243052
Yijun Ma <mayijun1203@gmail.com>
2020-08-26 21:38:18 -0400

    Create nlp.py

---
+++ nlp/nlp.py
@@ -0,0 +1,39 @@
+import nltk
+nltk.download()
+
+import nltk.book
+
+# Frequency
+dist=nltk.book.FreqDist(nltk.book.text7)
+dist.keys()
+
+
+# Stemmer
+k='List listed lists listing listings'
+k=k.lower().split(' ')
+porter=nltk.PorterStemmer()
+[porter.stem(t) for t in k]
+
+
+udhr=nltk.corpus.udhr.words('English-Latin1')
+porter=nltk.PorterStemmer()
+[porter.stem(t) for t in udhr[0:10]]
+
+
+
+
+# Lemmatization
+udhr=nltk.corpus.udhr.words('English-Latin1')
+wnl=nltk.WordNetLemmatizer()
+[wnl.lemmatize(t) for t in udhr[0:10]]
+
+
+# Tokenization
+k="Childerne shouldn't drink a sugary drink before bad."
+nltk.word_tokenize(k)
+
+
+
+# Sentence Splitting
+k="This is the first sentence. A gallon of milk in the U.S. costs $2.99. Is this the third sentence? Yes, it is!"
+nltk.sent_tokenize(k)
