72edf66dcabc806467e19abfee98f5377bce52e6
Yijun Ma <mayijun1203@gmail.com>
2020-08-31 23:56:58 -0400

    Update social network.py

--- social network/social network.py
+++ social network/social network.py
@@ -115,9 +115,31 @@ nx.hits(g)
 
 
 
+# Preferential Attachment Model
+# degree of the node/sum of degrees of all nodes
+g=nx.barabasi_albert_graph(1000000,1)
+d=g.degree()
+import pandas as pd
+h=pd.DataFrame(d)[1].hist(bins=1000,log=True)
 
 
 
+
+# Small World Model
+# Start with a ring and begin to rewire the edges randomly
+g=nx.watts_strogatz_graph(100,6,0.04)
+nx.draw_networkx(g)
+d=g.degree()
+import pandas as pd
+h=pd.DataFrame(d)[1].hist()
+nx.average_clustering(g)
+nx.average_shortest_path_length(g)
+# Connected Small World Network (run Watts up to t times till it returns a connected network)
+g=nx.connected_watts_strogatz_graph(100,6,0.04,50)
+nx.draw_networkx(g)
+# Newman Watts (adding new edges instead of rewiring)
+g=nx.newman_watts_strogatz_graph(100,6,0.04)
+nx.draw_networkx(g)
 
 
 
