422953c12fb8cc0b86f940a9faaea2c40a8664b0
Yijun Ma <mayijun1203@gmail.com>
2020-09-07 04:29:09 -0400

    update

--- htmlslide/plotly/plotly.py
+++ htmlslide/plotly/plotly.py
@@ -9,10 +9,10 @@ path='C:/Users/mayij/Desktop/DOC/GITHUB/MLGH/htmlslide/plotly/'
 
 
 df=pd.read_csv(path+'Subway_ridership_data_20200903.csv')
-df['Date']=df[::-1].reset_index(drop=True)
+df=df[::-1].reset_index(drop=True)
 
 
 fig=px.bar(df,x='Date', y='Total Estimated Ridership')
-fig.update_layout(autosize=True)
+# fig.update_layout(autosize=True)
 # fig.show()
 fig.write_html(path+'subway.html')
