9801232fcc753443977dfe015bab35d707ce4000
Yijun Ma <mayijun1203@gmail.com>
2020-09-22 03:34:23 -0400

    update

--- plotly/plotly.py
+++ plotly/plotly.py
@@ -4,6 +4,8 @@ import geopandas as gpd
 import plotly.io as pio
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.subplots as ps
+import json
 
 
 pd.set_option('display.max_columns', None)
@@ -78,6 +80,7 @@ fig.write_html('C:/Users/mayij/Desktop/DOC/GITHUB/td-covid19/report/plotly/turns
 
 
 # Mapping Full Settings
+# Point
 # px.set_mapbox_access_token(mapboxtoken)
 df=pd.read_csv(path+'cplxam.csv')
 df['DiffPct1'].describe(percentiles=np.arange(0.2,1,0.2))
@@ -90,7 +93,7 @@ fig=px.scatter_mapbox(df,
                       lat='CplxLat',
                       lon='CplxLong',
                       color='cat',
-                      text='CplxName',
+                      # text='Borough',
                       hover_name='CplxName',
                       hover_data={'CplxName':True,
                                   'CplxLat':False,
@@ -98,17 +101,101 @@ fig=px.scatter_mapbox(df,
                                   'Percent Change':(':.2f',df['DiffPct1']),
                                   'Percent Change Category':df['cat'],
                                   'cat':True},
-                      size=range(0,426),
-                      category_orders={'cat':['<-98%','-98%~-97%','-96%~-95%','-94%~-93%','-92%~-91%','>=-90%']},
+                      # size=range(0,426),
+                      category_orders={'cat':['>=-90%','-92%~-91%','-94%~-93%','-96%~-95%','<-96%']},
                       labels={'cat':'Percent Change Category'},
                       color_discrete_sequence=['#d1e3f3','#9ac8e1','#529dcc','#1c6cb1','#08306b'],
                       # color_discrete_map={'-94%~-93%':'#7f2704'},
-                      # opacity=0.9,
+                      opacity=0.9,
                       size_max=10,
                       zoom=9.5,
                       center={'lat':np.mean(df['CplxLat']),'lon':np.mean(df['CplxLong'])},
                       mapbox_style='carto-positron',
+                      title='Mapping Full Settings',
+                      template='ggplot2',
                       # height=800,
                       width=800)
 fig.show()
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.write_html(path+'cplxam.html',include_plotlyjs='cdn')
+
+
+
+
+# Line
+import json
+gdf=pd.read_json(path+'subway_line.geojson')
+points = []
+for  feature in gdf['features']:
+    if feature['geometry']['type'] == 'Polygon':
+        points.extend(feature['geometry']['coordinates'][0])    
+        points.append([None, None]) # mark the end of a polygon   
+        
+    elif feature['geometry']['type'] == 'MultiPolygon':
+        for polyg in feature['geometry']['coordinates']:
+            points.extend(polyg[0])
+            points.append([None, None]) #end of polygon
+    elif feature['geometry']['type'] == 'LineString': 
+        points.extend(feature['geometry']['coordinates'])
+        points.append([None, None])
+    elif feature['geometry']['type'] == 'MultiLineString':
+        for line  in feature['geometry']['coordinates']:
+            points.extend(line)
+            points.append([None, None])
+    else: pass  
+lons, lats = zip(*points)
+fig=px.line_mapbox(
+            lat=lats,
+            lon=lons,
+        )
+fig.show()
+
+
+gdf=gpd.read_file(path+'subway_line.geojson')
+gdf['geometry']=[x[0] for x in gdf['geometry']]
+gdf['lat']=[x.xy[1] for x in gdf['geometry']]
+gdf['lon']=[x.xy[0] for x in gdf['geometry']]
+fig=px.line_mapbox(gdf,
+                   lat='lat',
+                   lon='lon')
+fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# Polygon
+px.set_mapbox_access_token(mapboxtoken)
+gdf=gpd.read_file(path+'cplxpknta.shp')
+gdf.to_file(path+'cplxpknta.geojson',driver='GeoJSON',index=True)
+gdf=gpd.read_file(path+'cplxpknta.geojson')
+gjs=json.loads(gdf.to_json())
+fig=px.choropleth_mapbox(gdf,
+                         geojson=gjs,
+                         featureidkey='properties.index',
+                         locations='index',
+                         color='PKDiffPct3',
+                         center={'lat':np.mean([min(gdf.bounds['miny']),max(gdf.bounds['maxy'])]),
+                                 'lon':np.mean([min(gdf.bounds['minx']),max(gdf.bounds['maxx'])])},
+                         zoom=9.5,
+                         mapbox_style='carto-positron',
+                         title='Polygon',
+                         template='ggplot2',
+                         height=800,
+                         width=800
+                         )
+fig.show()
+fig.write_image(path+'cplxpknta.jpg',
+                width=800,
+                height=800,
+                scale=1,
+                validate=True,
+                engine='kaleido')
+
+
